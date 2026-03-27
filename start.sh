24109e995a54e228e3398cecfefb4ecba062d9a0
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-26 23:51:46 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Limpiar caché de rasa
+rm -rf .rasa/cache
+
 # Action server
 python -m rasa_sdk --actions actions --port 5055 &
 
 sleep 3
 
 # Entrena y arranca
-rasa train && rasa run --enable-api --cors "*" --port 10000
+rasa train --fixed-model-name modelo_nima && rasa run --enable-api --cors "*" --port 10000
