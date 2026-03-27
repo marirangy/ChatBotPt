9d33add9a51e9e15ffd978aec6b4b9d3cd1d753c
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 13:16:35 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ python -m rasa_sdk --actions actions --port 5055 &
 
 sleep 3
 
-# Entrena y arranca
-rasa train && rasa run --enable-api --cors "*" --port 10000
+rasa run --enable-api --cors "*" --port ${PORT:-10000}
