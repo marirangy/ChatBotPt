db8e039c4c7324453d4d89751a7404ae8937156b
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 17:11:25 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Action server
 python -m rasa_sdk --actions actions --port 5055 &
 
 sleep 3
 
-# Entrena usando /tmp para el caché (tiene permisos de escritura)
-RASA_HOME=/tmp rasa train --out /tmp/models_new && rasa run --enable-api --cors "*" --port 10000 --model /tmp/models_new
+HOME=/tmp rasa train --out /tmp/models_new && rasa run --enable-api --cors "*" --port 10000 --model /tmp/models_new
