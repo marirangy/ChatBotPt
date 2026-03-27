5016727ec22665bcd9cf00db0ad513ee684154c5
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-26 23:41:36 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ python -m rasa_sdk --actions actions --port 5055 &
 
 sleep 3
 
-# Rasa con el modelo específico
-rasa run --enable-api --cors "*" --port 10000 --model models/20260323-165757-coincident-world.tar.gz
+# Entrena y arranca
+rasa train && rasa run --enable-api --cors "*" --port 10000
