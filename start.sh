f58b6f0835308ea1cd732a673128eb27bbf05454
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 23:18:57 -0600

    Update start.sh prueba4

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-rasa run --enable-api --cors "*" --port 5005 -i 0.0.0.0 &
-RASA_PID=$!
-sleep 30
-python -m rasa_sdk --actions actions --port 5055
-wait $RASA_PID
+rasa run --enable-api --cors "*" --port 5005 -i 0.0.0.0
