d104cbbfd6b6791d8b098126f60e1984ace91d76
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 20:52:21 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,18 +3,7 @@
 # Action server
 python -m rasa_sdk --actions actions --port 5055 &
 
-# Entrena en background
-HOME=/tmp rasa train --out /tmp/models_new &
-TRAIN_PID=$!
+sleep 5
 
-# Inicia Rasa sin modelo para abrir el puerto mientras entrena
-rasa run --enable-api --cors "*" --port 10000 &
-RASA_PID=$!
-
-# Espera que termine el entrenamiento
-wait $TRAIN_PID
-
-# Mata el Rasa sin modelo y reinicia con el modelo entrenado
-kill $RASA_PID
-sleep 2
-rasa run --enable-api --cors "*" --port 10000 --model /tmp/models_new
+# Corre el modelo entrenado
+rasa run --enable-api --cors "*" --port 10000 -i 0.0.0.0
