e6b0c0632fdea3239a61664c1ede731f50d0c8b9
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 22:57:15 -0600

    prueba3

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rasa run --enable-api --cors "*" --port 10000 -i 0.0.0.0 &
-
-python -m rasa_sdk --actions actions --port 5055
+RASA_PID=$!
+sleep 30
+python -m rasa_sdk --actions actions --port 5055 &
+wait $RASA_PID
